19bc095b383d47a4aec58dae1c78b8445a914a33
Chari Gunasekaran <claxmana@splunk.com>
2020-02-26 22:41:45 -0800

    Array version

--- calc/math.go
+++ calc/math.go
@@ -1,6 +1,10 @@
 package calc
 
 //Returns sum of two integers
-func Add(i, j int) int {
-	return i + j
+func Add(i ...int) int {
+	sum := 0
+	for _, num := range i {
+		sum = sum + num
+	}
+	return sum
 }
